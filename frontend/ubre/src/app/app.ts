da1af41cfe54a9306b7797124ec39993267a8489
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-12-15 21:48:32 +0100

    [UPDATE] Added base global CSS styles (needs to be extended in the future)

--- frontend/ubre/src/app/app.ts
+++ frontend/ubre/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Map } from './map/map';
+import { Map } from './features/map/map';
 
 @Component({
   selector: 'app-root',
---
+++ frontend/ubre/src/app/shared/ui/button/button.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class ButtonComponent {
+  @Input() type: 'button' | 'submit' = 'button';
+  @Input() disabled = false;
+  @Input() variant: 'primary' | 'ghost' = 'primary';
+}
