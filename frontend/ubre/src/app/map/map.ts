4efaa467f05206b5b629f640b4b3a82a32b4209e
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-12-15 08:41:01 +0100

    [ADD] Integrate OpenStreetMap with Leaflet

---
+++ frontend/ubre/src/app/map/map.ts
@@ -0,0 +1,30 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  imports: [],
+  templateUrl: './map.html',
+  styleUrl: './map.css',
+})
+
+export class Map implements AfterViewInit {
+  private map!: L.Map;
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', { 
+      zoomControl : false, 
+      attributionControl : false,
+    }).setView([44.8200, 20.4481], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    }).addTo(this.map);
+
+    L.marker([44.8200, 20.4481]).addTo(this.map)
+  }
+}
