d9e0ee9f0c3cd34c3ca3cada9e9518339a1b95ed
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-12-22 17:54:07 +0100

    Update frontend files

--- frontend/ubre/src/app/app.routes.ts
+++ frontend/ubre/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { UserLayout } from './layouts/user-layout/user-layout';
-import { RegisteredUser } from './layouts/registered-user/registered-user';
 import { LoginComponent } from './features/auth/login/login.component';
 import { SignupComponent } from './features/auth/signup/signup.component';
 import { ForgotPassword } from './features/auth/forgot-password/forgot-password';
 import { ResetPassword } from './features/auth/reset-password/reset-password';
+import { User } from './models/user';
 
 export const routes: Routes = [
-  { path: '', component: RegisteredUser },
+  { path: '', component: UserLayout },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'forgot-password', component: ForgotPassword },
