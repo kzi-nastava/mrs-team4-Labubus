a97c4f0b9f603f0b1f7dde297810f7cc60cdf26e
NotixOfficial <gavric.sv84.2024@uns.ac.rs>
2025-12-22 17:34:44 +0100

    Created ui for signup

--- frontend/ubre/src/app/app.routes.ts
+++ frontend/ubre/src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { RegisteredUser } from './layouts/registered-user/registered-user';
+import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
+import { ForgotPassword } from './features/auth/forgot-password/forgot-password';
+import { ResetPassword } from './features/auth/reset-password/reset-password';
 
 export const routes: Routes = [
   { path: '', component: RegisteredUser },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: 'reset-password', component: ResetPassword },
 ];
