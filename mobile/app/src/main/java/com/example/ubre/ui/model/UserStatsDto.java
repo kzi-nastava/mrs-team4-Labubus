83827d1de80008625b523b5ce0cf9407a1ce37d8
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-12-25 15:28:17 +0100

    add: added dummy stat card (needs real time handler later)

--- mobile/app/src/main/java/com/example/ubre/ui/model/UserStatsDto.java
+++ mobile/app/src/main/java/com/example/ubre/ui/model/UserStatsDto.java
@@ -2,9 +2,13 @@ package com.example.ubre.ui.model;
 
 public class UserStatsDto { // extend later with other attributes
     private int activePast24Hours; // should be in minutes
+    private int distanceTraveled;
+    private int moneySpent;
 
-    public UserStatsDto(int activePast24Hours) {
+    public UserStatsDto(int activePast24Hours, int distanceTraveled, int moneySpent) {
         this.activePast24Hours = activePast24Hours;
+        this.distanceTraveled = distanceTraveled;
+        this.moneySpent = moneySpent;
     }
 
     public int getActivePast24Hours() {
@@ -13,5 +17,17 @@ public class UserStatsDto { // extend later with other attributes
 
     public void setActivePast24Hours(int activePast24Hours) {
         this.activePast24Hours = activePast24Hours;
+    }
+    public int getDistanceTraveled() {
+        return distanceTraveled;
+    }
+    public void setDistanceTraveled(int distanceTraveled) {
+        this.distanceTraveled = distanceTraveled;
+    }
+    public int getMoneySpent() {
+        return moneySpent;
+    }
+    public void setMoneySpent(int moneySpent) {
+        this.moneySpent = moneySpent;
     }
 }
