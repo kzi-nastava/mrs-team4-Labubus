0704d6ad51f1f61ba5b9929fb5b6445bafded656
NotixOfficial <mcnprodajax@gmail.com>
2025-12-26 23:41:35 +0100

    [ADD] Sprint 1 Retrospektiva

--- mobile/app/src/main/java/com/example/ubre/ui/main/MainActivity.java
+++ mobile/app/src/main/java/com/example/ubre/ui/main/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.core.view.GravityCompat;
@@ -44,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setDecorFitsSystemWindows(false);
+        // getWindow().setDecorFitsSystemWindows(false);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
         Configuration.getInstance().setUserAgentValue(getPackageName());
 
