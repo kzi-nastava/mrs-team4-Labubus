0f4037ef82287cfd1b4cba2c7599a9de19227e05
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-12-28 00:44:13 +0100

    add: global exception handler, from now on throw exceptions in service layer (with proper message), also extend handler with proper handler methods

--- backend/src/main/java/com/ubre/backend/controller/DriverController.java
+++ backend/src/main/java/com/ubre/backend/controller/DriverController.java
@@ -1,5 +1,6 @@
 package com.ubre.backend.controller;
 
+import com.ubre.backend.dto.DriverRegistrationDto;
 import com.ubre.backend.dto.UserDto;
 import com.ubre.backend.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,62 +12,79 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api/drivers")
+@RequestMapping("/api/driver")
 @CrossOrigin(origins = "*")
 public class DriverController {
 
     @Autowired
     private DriverService driverService;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<UserDto>> getAllDrivers() {
-        Collection<UserDto> drivers = driverService.getAllDrivers();
-        return new ResponseEntity<>(drivers, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/available", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<UserDto>> getAvailableDrivers() {
-        Collection<UserDto> drivers = driverService.getAvailableDrivers();
-        return new ResponseEntity<>(drivers, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDto> getDriverById(@PathVariable Long id) {
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> createDriver(@RequestBody DriverRegistrationDto driverRegistrationDto) {
         try {
-            UserDto driver = driverService.getDriverById(id);
-            return new ResponseEntity<>(driver, HttpStatus.OK);
+            UserDto driver = driverService.registerDriver(driverRegistrationDto);
+            return ResponseEntity.ok(driver);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
-    }
 
-    @PutMapping(value = "/{id}/toggle-availability")
-    public ResponseEntity<Void> toggleAvailability(@PathVariable Long id) {
-        try {
-            driverService.toggleAvailability(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
 
-    @PutMapping(value = "/{id}/approve-changes")
-    public ResponseEntity<Void> approveProfileChanges(@PathVariable Long id) {
-        try {
-            driverService.approveProfileChanges(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
 
-    @PutMapping(value = "/{id}/reject-changes")
-    public ResponseEntity<Void> rejectProfileChanges(@PathVariable Long id) {
-        try {
-            driverService.rejectProfileChanges(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+
+
     }
+//    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Collection<UserDto>> getAllDrivers() {
+//        Collection<UserDto> drivers = driverService.getAllDrivers();
+//        return new ResponseEntity<>(drivers, HttpStatus.OK);
+//    }
+//
+//    @GetMapping(value = "/available", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Collection<UserDto>> getAvailableDrivers() {
+//        Collection<UserDto> drivers = driverService.getAvailableDrivers();
+//        return new ResponseEntity<>(drivers, HttpStatus.OK);
+//    }
+//
+//    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<UserDto> getDriverById(@PathVariable Long id) {
+//        try {
+//            UserDto driver = driverService.getDriverById(id);
+//            return new ResponseEntity<>(driver, HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//    @PutMapping(value = "/{id}/toggle-availability")
+//    public ResponseEntity<Void> toggleAvailability(@PathVariable Long id) {
+//        try {
+//            driverService.toggleAvailability(id);
+//            return new ResponseEntity<>(HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//    @PutMapping(value = "/{id}/approve-changes")
+//    public ResponseEntity<Void> approveProfileChanges(@PathVariable Long id) {
+//        try {
+//            driverService.approveProfileChanges(id);
+//            return new ResponseEntity<>(HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+//
+//    @PutMapping(value = "/{id}/reject-changes")
+//    public ResponseEntity<Void> rejectProfileChanges(@PathVariable Long id) {
+//        try {
+//            driverService.rejectProfileChanges(id);
+//            return new ResponseEntity<>(HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
 }
---
+++ backend/src/main/java/com/ubre/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.ubre.backend.exception;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handle(BadRequestException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
