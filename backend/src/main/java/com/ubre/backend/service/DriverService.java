5506372a45254563a4d675944463b806930a0bc8
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-12-27 13:30:00 +0100

    Added DriverService (shell)

--- backend/src/main/java/com/ubre/backend/service/DriverService.java
+++ backend/src/main/java/com/ubre/backend/service/DriverService.java
@@ -15,25 +15,3 @@ public interface DriverService {
     void rejectProfileChanges(Long driverId);
 }
 
-// Inner class for registerDriver method
-class RegisterDTO {
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String phoneNumber;
-    private String drivingLicense;
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-    public String getDrivingLicense() { return drivingLicense; }
-    public void setDrivingLicense(String drivingLicense) { this.drivingLicense = drivingLicense; }
-}
