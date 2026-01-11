8c7b97de7d0c49fe7661335fbe6b32a899a733a2
miljan-jokic <jokic.sv59.2023@uns.ac.rs>
2026-01-11 17:27:14 +0100

    [FIX] Changed the throws declaration in VehicleService interface to correct class of exception

--- backend/src/main/java/com/ubre/backend/service/VehicleService.java
+++ backend/src/main/java/com/ubre/backend/service/VehicleService.java
@@ -4,16 +4,17 @@ import com.ubre.backend.dto.VehicleDto;
 import com.ubre.backend.dto.VehicleIndicatorDto;
 import com.ubre.backend.dto.WaypointDto;
 import org.springframework.web.ErrorResponseException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 
 public interface VehicleService {
-    VehicleDto createVehicle(VehicleDto createVehicleDto, Long userId) throws ErrorResponseException;
-    VehicleDto getVehicleById(Long id) throws ErrorResponseException;
-    VehicleDto getVehicleByDriver(Long driverId) throws ErrorResponseException;
-    VehicleDto updateVehicle(Long id, VehicleDto updateVehicleDto) throws ErrorResponseException;
-    VehicleDto deleteVehicle(Long id) throws ErrorResponseException;
+    VehicleDto createVehicle(VehicleDto createVehicleDto, Long userId) throws ResponseStatusException;
+    VehicleDto getVehicleById(Long id) throws ResponseStatusException;
+    VehicleDto getVehicleByDriver(Long driverId) throws ResponseStatusException;
+    VehicleDto updateVehicle(Long id, VehicleDto updateVehicleDto) throws ResponseStatusException;
+    VehicleDto deleteVehicle(Long id) throws ResponseStatusException;
     Collection<VehicleIndicatorDto> getVehicleIndicators();
-    VehicleIndicatorDto getVehicleIndicator(Long id) throws ErrorResponseException;
-    VehicleIndicatorDto setVehicleIndicator(Long id, WaypointDto waypointDto) throws ErrorResponseException;
+    VehicleIndicatorDto getVehicleIndicator(Long id) throws ResponseStatusException;
+    VehicleIndicatorDto setVehicleIndicator(Long id, WaypointDto waypointDto) throws ResponseStatusException;
 }
