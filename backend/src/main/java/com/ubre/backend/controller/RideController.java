a0744a72489981608ed5133de046a96c8aa60576
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2026-01-11 21:29:37 +0100

    update: api routes are fixed

--- backend/src/main/java/com/ubre/backend/controller/RideController.java
+++ backend/src/main/java/com/ubre/backend/controller/RideController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ride")
+@RequestMapping("/api/rides")
 @CrossOrigin(origins = "*")
 public class RideController {
 
