bc450fcfaf6744513b1b7146700020e693393c7a
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2026-01-16 04:34:33 +0100

    update: new changes, forbidden and unauthorized

--- backend/src/main/java/com/ubre/backend/controller/DriverController.java
+++ backend/src/main/java/com/ubre/backend/controller/DriverController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -29,6 +30,7 @@ public class DriverController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UserDto> createDriver(@RequestBody DriverRegistrationDto driverRegistrationDto) {
         UserDto createdDriver = driverService.createDriver(driverRegistrationDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdDriver);
