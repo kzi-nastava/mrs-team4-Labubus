370a5d0e55dd1d0ac8a8d7910452039a7808054e
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2026-01-18 00:00:52 +0100

    update: new repository added - profile change

---
+++ backend/src/main/java/com/ubre/backend/repository/ProfileChangeRepository.java
@@ -0,0 +1,17 @@
+package com.ubre.backend.repository;
+
+import com.ubre.backend.enums.UserStatus;
+import com.ubre.backend.model.Driver;
+import com.ubre.backend.model.ProfileChange;
+import com.ubre.backend.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProfileChangeRepository extends JpaRepository<ProfileChange, Long> {
+
+}
