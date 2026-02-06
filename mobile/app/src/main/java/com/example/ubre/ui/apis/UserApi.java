ddf0a32708925fbf9eceb13b08d2d83cb9d1edee
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2026-02-06 14:10:46 +0100

    update: user profiles

--- mobile/app/src/main/java/com/example/ubre/ui/apis/UserApi.java
+++ mobile/app/src/main/java/com/example/ubre/ui/apis/UserApi.java
@@ -6,12 +6,13 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 public interface UserApi {
     @GET("api/users/{id}")
-    Call<UserDto> getUserById(@Path("id") Long id);
+    Call<UserDto> getUserById(@Header("Authorization") String authHeader, @Path("id") Long id);
 
     // get user avatar
     @GET("api/users/{id}/avatar") // on frontend, we used blob, but now we use response body to get the image
--- mobile/app/src/main/java/com/example/ubre/ui/services/UserService.java
+++ mobile/app/src/main/java/com/example/ubre/ui/services/UserService.java
@@ -1,65 +1,54 @@
 package com.example.ubre.ui.services;
 
-// main difference between this service, and angular serices, is that there is no logic here, only state management, and api calls with repositories is separate class.
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 
+import com.example.ubre.ui.apis.ApiClient;
+import com.example.ubre.ui.apis.UserApi;
 import com.example.ubre.ui.dtos.UserDto;
+import com.example.ubre.ui.main.MainActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserService {
-
+    private static final String TAG = "UserService";
     private static UserService instance;
-    private MutableLiveData<UserDto> currentUser = new MutableLiveData<>();
-    private MutableLiveData<byte[]> currentUserAvatar = new MutableLiveData<>();
 
     private UserService() {
     }
 
-    public static synchronized UserService getInstance() {
+    public static UserService getInstance() {
         if (instance == null) {
             instance = new UserService();
         }
         return instance;
     }
 
-    public MutableLiveData<UserDto> getCurrentUser() {
-        return currentUser;
-    }
-
-    public LiveData<UserDto> getCurrentUserReadOnly() {
-        return currentUser;
-    }
-
-    public void setCurrentUser(UserDto user) {
-        currentUser.setValue(user);
-    }
-
-    public void updateCurrentUser(UserDto updated) {
-        if (updated != null) {
-            currentUser.setValue(updated);
-        }
-    }
-    public void clearCurrentUser() {
-        currentUser.setValue(null);
-    }
-
-    public boolean isLoggedIn() {
-        return currentUser.getValue() != null;
-    }
-
-    public MutableLiveData<byte[]> getCurrentUserAvatar() {
-        return currentUserAvatar;
-    }
-
-    public LiveData<byte[]> getCurrentUserAvatarReadOnly() {
-        return currentUserAvatar;
-    }
-
-    public void setCurrentUserAvatar(byte[] avatar) {
-        currentUserAvatar.setValue(avatar);
-    }
-
-    public void clearCurrentUserAvatar() {
-        currentUserAvatar.setValue(null);
+    public void getUserById() throws Exception {
+//        UserApi userApi = ApiClient.getClient().create(UserApi.class);
+//        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("app_prefs", Context.MODE_PRIVATE);
+//        String token = sharedPreferences.getString("jwt", null);
+//
+//        if (token == null) {
+//            throw new Exception("User not authenticated");
+//        }
+//
+//        userApi.getUserById("Bearer " + token, ).enqueue(new Callback<UserDto>() {
+//            @Override
+//            public void onResponse(Call<UserDto> call, Response<UserDto> response) {
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<UserDto> call, Throwable t) {
+//
+//            }
+//        });
     }
 }
