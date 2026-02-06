622ddb2761ad94dd105377fd9c1e191808181719
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2026-02-06 01:14:06 +0100

    update: user repository changes

--- mobile/app/src/main/java/com/example/ubre/ui/repositories/UserRepository.java
+++ mobile/app/src/main/java/com/example/ubre/ui/repositories/UserRepository.java
@@ -1,7 +1,24 @@
 package com.example.ubre.ui.repositories;
 
+import com.example.ubre.ui.dtos.UserDto;
+
 public class UserRepository {
     private static final String TAG = "UserRepository";
+    private static UserRepository instance;
+
+    private UserRepository() {
+    }
+
+    public static UserRepository getInstance() {
+        if (instance == null) {
+            instance = new UserRepository();
+        }
+        return instance;
+    }
+
+    public UserDto getUserProfile() { // after login, we fetch user profile data to display in profile screen and use in other parts of the app
+
+    }
 
 
 }
