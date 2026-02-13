e0b1d05fd26d07e47e39f5068fdcb2d1d8ca10a9
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2026-02-13 01:44:20 +0100

    update: add e2e tests for user page (base for everyone)

--- e2e/src/test/java/e2e/pages/GuestPage.java
+++ e2e/src/test/java/e2e/pages/GuestPage.java
@@ -4,14 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class GuestPage {
-
-//    WebElement openMenu = driver.findElement(By.cssSelector("[data-testid='open-side-menu']"));
-//    WebElement email = driver.findElement(By.cssSelector("[data-testid='login-email']"));
-//    WebElement password = driver.findElement(By.cssSelector("[data-testid='login-password']"));
-//    WebElement submit = driver.findElement(By.cssSelector("[data-testid='login-submit']"));
-//    WebElement sideMenuLogin = driver.findElement(By.cssSelector("[data-testid='side-menu-login']"));
-
     private final WebDriver driver;
+    private final By openMenuButton = By.cssSelector("[data-testid='open-side-menu']");
+    private final By sideMenuLoginButton = By.cssSelector("[data-testid='side-menu-login']");
+    private final By sideMenuUserName = By.cssSelector("[data-testid='side-menu-user-name']");
 
     public GuestPage(WebDriver driver) {
         this.driver = driver;
@@ -19,5 +15,22 @@ public class GuestPage {
 
     public boolean isDisplayed() {
         return driver.findElement(By.tagName("body")).isDisplayed();
+    }
+
+    public boolean isSideMenuButtonDisplayed() {
+        return driver.findElement(openMenuButton).isDisplayed();
+    }
+
+    public void openSideMenu() {
+        driver.findElement(openMenuButton).click();
+    }
+
+    public LoginPage goToLogin() {
+        driver.findElement(sideMenuLoginButton).click();
+        return new LoginPage(driver);
+    }
+
+    public String getSideMenuUserNameText() {
+        return driver.findElement(sideMenuUserName).getText();
     }
 }
