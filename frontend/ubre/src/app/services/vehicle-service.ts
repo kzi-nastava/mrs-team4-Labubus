ee628360e51edaded37f6ace67a351402c4dd181
miljan-jokic <jokic.sv59.2023@uns.ac.rs>
2026-02-19 05:10:16 +0100

    [ADD] Vehicle location sending on frontend and ride simulation

--- frontend/ubre/src/app/services/vehicle-service.ts
+++ frontend/ubre/src/app/services/vehicle-service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, take } from 'rxjs';
 import { VehicleDto } from '../dtos/vehicle-dto';
 import { WebSocketService } from './websocket-service';
 import { VehicleIndicatorDto } from '../dtos/vehicle-indicator-dto';
@@ -10,6 +10,10 @@ import { RidePlanningStore } from './ride-planning/ride-planning-store';
 import { RideStatus } from '../enums/ride-status';
 import { WaypointDto } from '../dtos/waypoint-dto';
 import { RideDto } from '../dtos/ride-dto';
+import { UserService } from './user-service';
+import { UserDto } from '../dtos/user-dto';
+import { Role } from '../enums/role';
+import { RouteInfo } from './ride-planning/ride-types';
 
 @Injectable({
   providedIn: 'root',
@@ -21,12 +25,16 @@ export class VehicleService {
     private readonly webSocketService : WebSocketService = inject(WebSocketService);
     private readonly rideTrackingStore : RideTrackingStore = inject(RideTrackingStore);
     private readonly ridePlanningStore : RidePlanningStore = inject(RidePlanningStore);
+    private readonly userService : UserService = inject(UserService);
 
     // Vehicle service handles the vehicle locations and listens to the websocket topic for them. Not sure if this organiztion is ok.
     private vehicleLocationsSubscribtion? : Subscription;
 
     private vehicleLocations : BehaviorSubject<VehicleIndicatorDto[]> = new BehaviorSubject<VehicleIndicatorDto[]>([])
     readonly vehicleLocations$ = this.vehicleLocations.asObservable();
+
+    private locationSendInterval : number | null = null;
+    private currentDriverVehicle : VehicleDto | null = null;
 
     constructor() {
       this.webSocketService.connect();
@@ -58,6 +66,22 @@ export class VehicleService {
           }
         }
       })
+
+      this.userService.currentUser$.subscribe((currentUser : UserDto) => {
+        if (currentUser.role == Role.DRIVER) {
+          this.getVehicleByDriver(currentUser.id).pipe(take(1)).subscribe((currentVehicle : VehicleDto) => {
+            this.currentDriverVehicle = currentVehicle
+            this.locationSendInterval = setInterval(() => {this.sendLocation()}, 1000)
+          })
+        }
+        else {
+          this.currentDriverVehicle = null
+          if (this.locationSendInterval) {
+            clearInterval(this.locationSendInterval)
+            this.locationSendInterval = null
+          }
+        }
+      })
     }
 
     ngOnDestroy() {
@@ -73,4 +97,64 @@ export class VehicleService {
       return this.http.get<VehicleDto>(`${this.BASE_URL}vehicles/driver/${driverId}`);
     }
 
+    // ACTUAL SEND LOCATION IMPLEMENTATION (USING DIFFERENT ONE FOR BETTER SHOWCASE)
+    // public sendLocation() {
+    //   if (!navigator.geolocation) {
+    //         return;
+    //     }
+
+    //     navigator.geolocation.getCurrentPosition(
+    //         (pos) => {
+    //             const { latitude, longitude } = pos.coords;
+    //             const id = Date.now();
+    //             const fallback = `${latitude.toFixed(5)}, ${longitude.toFixed(5)}`;
+
+    //             const newWaypoint: WaypointDto = {
+    //                 id: Number(id),
+    //                 label: fallback,
+    //                 latitude: latitude,
+    //                 longitude: longitude,
+    //                 visited: false
+    //             };
+
+    //             return this.http.post<VehicleDto>(`${this.BASE_URL}vehicles/${this.currentDriverVehicle?.id}/location`, newWaypoint).pipe(take(1)).subscribe(()=>{});
+    //         },
+    //         (error) => {
+    //             console.error('Error getting current location:', error);
+    //         }
+    //     );
+    // }
+
+    public sendLocation() {
+      this.ridePlanningStore.currentRide$.pipe(take(1)).subscribe((currentRide : RideDto | null) => {
+        if (currentRide == null || currentRide.status != RideStatus.IN_PROGRESS)
+          return;
+
+        this.rideTrackingStore.trackedRoute$.pipe(take(1)).subscribe((route : RouteInfo) => {
+          if (route.geometry.coordinates.length < 2)
+            return;
+
+          let coordinate = route.geometry.coordinates[0]
+          for (let c of route.geometry.coordinates) {
+            if (c[0] != coordinate[0] || c[1] != coordinate[1]) {
+              coordinate = c;
+              break;
+            }
+          }
+
+          if (route.geometry.coordinates[0][0] == coordinate[0] && route.geometry.coordinates[0][1] == coordinate[1])
+            return
+
+          const id = Date.now();
+          const newWaypoint: WaypointDto = {
+              id: Number(id),
+              label: "",
+              latitude: coordinate[1],
+              longitude: coordinate[0],
+              visited: false
+          };
+          return this.http.post<VehicleDto>(`${this.BASE_URL}vehicles/${this.currentDriverVehicle?.id}/location`, newWaypoint).pipe(take(1)).subscribe(()=>{});
+        })
+      })
+    }
 }
